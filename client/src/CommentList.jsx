a995bc96fd70c9e4b5fdff929460ef0f3bfa3526
Shivam <smshivammishra1998@gmail.com>
2023-04-08 17:51:31 +0530

    event sync

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 function CommentList({ comments }) {
-
     return (
         <div className='d-flex flex-row flex-wrap justify-content-betwwen'>
             {
-                comments.map(comment => (
-                    <li key={comment.id}>{comment.content}</li>
-                ))
+                comments.map(comment => {
+                    const { id, status, content } = comment;
+                    if (status === 'approved')
+                        return <li style={{ color: 'green' }} key={id}>{content}</li>
+                    if (status === 'rejected')
+                        return <li style={{ color: 'red' }} key={id}>Comment is rejected</li>
+                    return <li key={id}>Comment is pending moderation</li>
+                })
             }
         </div>
     )
